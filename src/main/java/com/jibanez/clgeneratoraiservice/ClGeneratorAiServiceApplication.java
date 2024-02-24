a65b0bd194189bc76e61d525762dec782af960fb
JhonatanIT <jhonatan.ibatac@gmail.com>
2024-02-24 21:23:13 +1030

    build: initial commit

---
+++ src/main/java/com/jibanez/clgeneratoraiservice/ClGeneratorAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jibanez.clgeneratoraiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClGeneratorAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClGeneratorAiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jibanez/clgeneratoraiservice/ClGeneratorAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibanez.clgeneratoraiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClGeneratorAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
